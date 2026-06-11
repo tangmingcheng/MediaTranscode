9f38344ab3b03e34ea82e7d885c2f32216875a90
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-11 16:36:56 +0800

    refactor: add internal FFmpeg utility declarations

---
+++ src/internal/FFmpegUtils.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "media_transcode/MediaTranscodeTypes.h"
+
+#include <cstdint>
+#include <string>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/version.h>
+}
+
+namespace media::ffmpeg {
+
+    std::string errorString(int err);
+
+    const char* preferredVideoEncoderName(VideoCodec codec);
+    AVCodecID fallbackVideoCodecId(VideoCodec codec);
+
+    int normalizeEvenSize(int value);
+    int chooseOutputFps(const TranscodeConfig& config, const AVStream* inputVideoStream);
+
+    AVPixelFormat chooseVideoEncoderPixelFormat(const AVCodec* encoder);
+    AVSampleFormat chooseAudioSampleFormat(const AVCodec* encoder);
+    int chooseAudioSampleRate(const AVCodec* encoder, int preferredRate);
+
+    int audioChannelCount(const AVCodecContext* ctx);
+    bool ensureAudioDecoderChannelLayout(AVCodecContext* ctx);
+    bool copyAudioChannelLayoutToEncoder(AVCodecContext* encoderCtx,
+                                         const AVCodecContext* decoderCtx);
+    bool setFrameAudioLayoutFromCodecContext(AVFrame* frame,
+                                             const AVCodecContext* codecCtx);
+
+#if LIBAVUTIL_VERSION_MAJOR < 57
+    int64_t oldAudioChannelLayout(const AVCodecContext* ctx);
+#endif
+
+    bool isHardwareEncoderName(const char* name);
+    void setVideoEncoderOptions(AVCodecContext* encoderCtx, const AVCodec* encoder);
+
+} // namespace media::ffmpeg
