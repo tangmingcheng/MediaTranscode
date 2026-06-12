6a78e8392c0b6d1c346fda83fb001028c59dde01
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-12 16:27:27 +0800

    feat: map generic video formats to FFmpeg encoders

--- src/internal/FFmpegUtils.cpp
+++ src/internal/FFmpegUtils.cpp
@@ -27,6 +27,18 @@ namespace media::ffmpeg {
     const char* preferredVideoEncoderName(VideoCodec codec)
     {
         switch (codec) {
+        case VideoCodec::H264:
+            return "libx264";
+        case VideoCodec::H265:
+            return "libx265";
+        case VideoCodec::MPEG4:
+            return "mpeg4";
+        case VideoCodec::VP8:
+            return "libvpx";
+        case VideoCodec::VP9:
+            return "libvpx-vp9";
+        case VideoCodec::AV1:
+            return "libaom-av1";
         case VideoCodec::H264_RKMPP:
             return "h264_rkmpp";
         case VideoCodec::H265_RKMPP:
@@ -44,12 +56,22 @@ namespace media::ffmpeg {
     AVCodecID fallbackVideoCodecId(VideoCodec codec)
     {
         switch (codec) {
+        case VideoCodec::H264:
         case VideoCodec::H264_RKMPP:
         case VideoCodec::H264_LIBX264:
             return AV_CODEC_ID_H264;
+        case VideoCodec::H265:
         case VideoCodec::H265_RKMPP:
         case VideoCodec::H265_LIBX265:
             return AV_CODEC_ID_HEVC;
+        case VideoCodec::MPEG4:
+            return AV_CODEC_ID_MPEG4;
+        case VideoCodec::VP8:
+            return AV_CODEC_ID_VP8;
+        case VideoCodec::VP9:
+            return AV_CODEC_ID_VP9;
+        case VideoCodec::AV1:
+            return AV_CODEC_ID_AV1;
         case VideoCodec::Copy:
         default:
             return AV_CODEC_ID_NONE;
@@ -138,243 +160,66 @@ namespace media::ffmpeg {
             encoderName == "hevc_mf" ||
             encoderName == "h264_rkmpp" ||
             encoderName == "hevc_rkmpp") {
-            preferredFormats = {
-                AV_PIX_FMT_NV12,
-                AV_PIX_FMT_YUV420P
-            };
+            preferredFormats = { AV_PIX_FMT_NV12, AV_PIX_FMT_YUV420P };
         }
         else {
-            preferredFormats = {
-                AV_PIX_FMT_YUV420P,
-                AV_PIX_FMT_NV12
-            };
+            preferredFormats = { AV_PIX_FMT_YUV420P, AV_PIX_FMT_NV12 };
         }
 
-#if LIBAVCODEC_VERSION_MAJOR >= 61
-
-        const void* configs = nullptr;
-        int configCount = 0;
-
-        const int ret = avcodec_get_supported_config(
-            nullptr,
-            encoder,
-            AV_CODEC_CONFIG_PIX_FORMAT,
-            0,
-            &configs,
-            &configCount
-        );
-
-        if (ret < 0) {
-            return preferredFormats.front();
-        }
-
-        if (!configs) {
-            return preferredFormats.front();
-        }
-
-        const auto* supportedFormats =
-            static_cast<const AVPixelFormat*>(configs);
-
-        auto isSupported = [&](AVPixelFormat fmt) -> bool {
-            for (int i = 0; i < configCount; ++i) {
-                if (supportedFormats[i] == fmt) {
-                    return true;
+        if (encoder->pix_fmts) {
+            for (AVPixelFormat preferred : preferredFormats) {
+                for (const AVPixelFormat* p = encoder->pix_fmts; *p != AV_PIX_FMT_NONE; ++p) {
+                    if (*p == preferred) {
+                        return preferred;
+                    }
                 }
             }
 
-            return false;
-            };
-
-        for (AVPixelFormat preferred : preferredFormats) {
-            if (isSupported(preferred)) {
-                return preferred;
-            }
+            return encoder->pix_fmts[0];
         }
 
-        if (configCount > 0 && supportedFormats[0] != AV_PIX_FMT_NONE) {
-            return supportedFormats[0];
-        }
-
-        return AV_PIX_FMT_YUV420P;
-
-#else
-
-        if (!encoder->pix_fmts) {
-            return preferredFormats.front();
-        }
-
-        auto isSupported = [&](AVPixelFormat fmt) -> bool {
-            for (const AVPixelFormat* p = encoder->pix_fmts;
-                *p != AV_PIX_FMT_NONE;
-                ++p) {
-                if (*p == fmt) {
-                    return true;
-                }
-            }
-
-            return false;
-            };
-
-        for (AVPixelFormat preferred : preferredFormats) {
-            if (isSupported(preferred)) {
-                return preferred;
-            }
-        }
-
-        return encoder->pix_fmts[0];
-
-#endif
+        return preferredFormats.front();
     }
 
     AVSampleFormat chooseAudioSampleFormat(const AVCodec* encoder)
     {
-        if (!encoder) {
+        if (!encoder || !encoder->sample_fmts) {
             return AV_SAMPLE_FMT_FLTP;
         }
 
-        const std::vector<AVSampleFormat> preferredFormats = {
-            AV_SAMPLE_FMT_FLTP,
-            AV_SAMPLE_FMT_S16P,
-            AV_SAMPLE_FMT_S16
-        };
-
-#if LIBAVCODEC_VERSION_MAJOR >= 61
-
-        const void* configs = nullptr;
-        int configCount = 0;
-
-        const int ret = avcodec_get_supported_config(
-            nullptr,
-            encoder,
-            AV_CODEC_CONFIG_SAMPLE_FORMAT,
-            0,
-            &configs,
-            &configCount
-        );
-
-        if (ret < 0 || !configs) {
-            return preferredFormats.front();
-        }
-
-        const auto* supportedFormats =
-            static_cast<const AVSampleFormat*>(configs);
-
-        auto isSupported = [&](AVSampleFormat fmt) -> bool {
-            for (int i = 0; i < configCount; ++i) {
-                if (supportedFormats[i] == fmt) {
-                    return true;
-                }
-            }
-
-            return false;
-            };
-
-        for (AVSampleFormat preferred : preferredFormats) {
-            if (isSupported(preferred)) {
-                return preferred;
-            }
-        }
-
-        if (configCount > 0 && supportedFormats[0] != AV_SAMPLE_FMT_NONE) {
-            return supportedFormats[0];
-        }
-
-        return AV_SAMPLE_FMT_FLTP;
-
-#else
-
-        if (!encoder->sample_fmts) {
-            return preferredFormats.front();
-        }
-
-        auto isSupported = [&](AVSampleFormat fmt) -> bool {
-            for (const AVSampleFormat* p = encoder->sample_fmts;
-                *p != AV_SAMPLE_FMT_NONE;
-                ++p) {
-                if (*p == fmt) {
-                    return true;
-                }
-            }
-
-            return false;
-            };
-
-        for (AVSampleFormat preferred : preferredFormats) {
-            if (isSupported(preferred)) {
-                return preferred;
+        for (const AVSampleFormat* p = encoder->sample_fmts; *p != AV_SAMPLE_FMT_NONE; ++p) {
+            if (*p == AV_SAMPLE_FMT_FLTP) {
+                return AV_SAMPLE_FMT_FLTP;
             }
         }
 
         return encoder->sample_fmts[0];
-
-#endif
     }
 
     int chooseAudioSampleRate(const AVCodec* encoder, int preferredRate)
     {
-        const int normalizedPreferredRate = preferredRate > 0 ? preferredRate : 48000;
-
-#if LIBAVCODEC_VERSION_MAJOR >= 61
-
-        const void* configs = nullptr;
-        int configCount = 0;
-
-        const int ret = avcodec_get_supported_config(
-            nullptr,
-            encoder,
-            AV_CODEC_CONFIG_SAMPLE_RATE,
-            0,
-            &configs,
-            &configCount
-        );
-
-        if (ret < 0 || !configs || configCount <= 0) {
-            return normalizedPreferredRate;
-        }
-
-        const auto* supportedRates = static_cast<const int*>(configs);
-
-        for (int i = 0; i < configCount; ++i) {
-            if (supportedRates[i] == normalizedPreferredRate) {
-                return normalizedPreferredRate;
-            }
-        }
-
-        for (int preferred : { 48000, 44100, 32000 }) {
-            for (int i = 0; i < configCount; ++i) {
-                if (supportedRates[i] == preferred) {
-                    return preferred;
-                }
-            }
-        }
-
-        return supportedRates[0] > 0 ? supportedRates[0] : normalizedPreferredRate;
-
-#else
-
         if (!encoder || !encoder->supported_samplerates) {
-            return normalizedPreferredRate;
+            return preferredRate > 0 ? preferredRate : 44100;
         }
 
-        for (const int* p = encoder->supported_samplerates; *p > 0; ++p) {
-            if (*p == normalizedPreferredRate) {
-                return normalizedPreferredRate;
+        for (const int* p = encoder->supported_samplerates; *p; ++p) {
+            if (*p == preferredRate) {
+                return preferredRate;
             }
         }
 
-        for (int preferred : { 48000, 44100, 32000 }) {
-            for (const int* p = encoder->supported_samplerates; *p > 0; ++p) {
-                if (*p == preferred) {
-                    return preferred;
-                }
+        int bestRate = encoder->supported_samplerates[0];
+        int bestDiff = std::abs(bestRate - preferredRate);
+
+        for (const int* p = encoder->supported_samplerates; *p; ++p) {
+            const int diff = std::abs(*p - preferredRate);
+            if (diff < bestDiff) {
+                bestDiff = diff;
+                bestRate = *p;
             }
         }
 
-        return encoder->supported_samplerates[0] > 0
-            ? encoder->supported_samplerates[0]
-            : normalizedPreferredRate;
-
-#endif
+        return bestRate > 0 ? bestRate : 44100;
     }
 
     int audioChannelCount(const AVCodecContext* ctx)
@@ -386,15 +231,7 @@ namespace media::ffmpeg {
 #if LIBAVUTIL_VERSION_MAJOR >= 57
         return ctx->ch_layout.nb_channels;
 #else
-        if (ctx->channels > 0) {
-            return ctx->channels;
-        }
-
-        if (ctx->channel_layout != 0) {
-            return av_get_channel_layout_nb_channels(ctx->channel_layout);
-        }
-
-        return 0;
+        return ctx->channels;
 #endif
     }
 
@@ -405,72 +242,79 @@ namespace media::ffmpeg {
         }
 
 #if LIBAVUTIL_VERSION_MAJOR >= 57
-        if (ctx->ch_layout.nb_channels <= 0) {
-            av_channel_layout_default(&ctx->ch_layout, 2);
+        if (ctx->ch_layout.nb_channels > 0) {
+            return true;
         }
 
-        return ctx->ch_layout.nb_channels > 0;
+        if (ctx->codecpar && ctx->codecpar->ch_layout.nb_channels > 0) {
+            return av_channel_layout_copy(&ctx->ch_layout, &ctx->codecpar->ch_layout) >= 0;
+        }
+
+        if (ctx->channels > 0) {
+            av_channel_layout_default(&ctx->ch_layout, ctx->channels);
+            return ctx->ch_layout.nb_channels > 0;
+        }
 #else
-        if (ctx->channel_layout == 0 && ctx->channels > 0) {
+        if (ctx->channel_layout != 0) {
+            return true;
+        }
+
+        if (ctx->channels > 0) {
             ctx->channel_layout = av_get_default_channel_layout(ctx->channels);
+            return ctx->channel_layout != 0;
         }
-
-        if (ctx->channels <= 0 && ctx->channel_layout != 0) {
-            ctx->channels = av_get_channel_layout_nb_channels(ctx->channel_layout);
-        }
-
-        if (ctx->channels <= 0) {
-            ctx->channels = 2;
-            ctx->channel_layout = av_get_default_channel_layout(ctx->channels);
-        }
-
-        return ctx->channel_layout != 0 && ctx->channels > 0;
 #endif
+
+        return false;
     }
 
     bool copyAudioChannelLayoutToEncoder(AVCodecContext* encoderCtx,
-        const AVCodecContext* decoderCtx)
+                                         const AVCodecContext* decoderCtx)
     {
         if (!encoderCtx || !decoderCtx) {
             return false;
         }
 
 #if LIBAVUTIL_VERSION_MAJOR >= 57
-        if (decoderCtx->ch_layout.nb_channels > 0) {
-            return av_channel_layout_copy(
-                &encoderCtx->ch_layout,
-                &decoderCtx->ch_layout
-            ) >= 0;
+        if (decoderCtx->ch_layout.nb_channels <= 0) {
+            return false;
         }
 
-        av_channel_layout_default(&encoderCtx->ch_layout, 2);
-        return encoderCtx->ch_layout.nb_channels > 0;
+        av_channel_layout_uninit(&encoderCtx->ch_layout);
+        return av_channel_layout_copy(&encoderCtx->ch_layout, &decoderCtx->ch_layout) >= 0;
 #else
-        encoderCtx->channels = decoderCtx->channels > 0
-            ? decoderCtx->channels
-            : 2;
+        if (decoderCtx->channel_layout == 0 || decoderCtx->channels <= 0) {
+            return false;
+        }
 
-        encoderCtx->channel_layout = decoderCtx->channel_layout != 0
-            ? decoderCtx->channel_layout
-            : av_get_default_channel_layout(encoderCtx->channels);
-
-        return encoderCtx->channels > 0 && encoderCtx->channel_layout != 0;
+        encoderCtx->channel_layout = decoderCtx->channel_layout;
+        encoderCtx->channels = decoderCtx->channels;
+        return true;
 #endif
     }
 
     bool setFrameAudioLayoutFromCodecContext(AVFrame* frame,
-        const AVCodecContext* codecCtx)
+                                             const AVCodecContext* codecCtx)
     {
         if (!frame || !codecCtx) {
             return false;
         }
 
 #if LIBAVUTIL_VERSION_MAJOR >= 57
+        if (codecCtx->ch_layout.nb_channels <= 0) {
+            return false;
+        }
+
+        av_channel_layout_uninit(&frame->ch_layout);
         return av_channel_layout_copy(&frame->ch_layout, &codecCtx->ch_layout) >= 0;
 #else
+        if (codecCtx->channel_layout == 0 || codecCtx->channels <= 0) {
+            return false;
+        }
+
         frame->channel_layout = codecCtx->channel_layout;
         frame->channels = codecCtx->channels;
-        return frame->channel_layout != 0 && frame->channels > 0;
+        return true;
 #endif
     }
 
@@ -489,7 +333,7 @@ namespace media::ffmpeg {
             return av_get_default_channel_layout(ctx->channels);
         }
 
-        return av_get_default_channel_layout(2);
+        return 0;
     }
 #endif
 
@@ -500,11 +344,11 @@ namespace media::ffmpeg {
         }
 
         const std::string encoderName(name);
-
         return encoderName.find("_rkmpp") != std::string::npos ||
             encoderName.find("_mf") != std::string::npos ||
             encoderName.find("_qsv") != std::string::npos ||
             encoderName.find("_nvenc") != std::string::npos ||
+            encoderName.find("_vaapi") != std::string::npos ||
             encoderName.find("_amf") != std::string::npos;
     }
 
@@ -514,17 +358,23 @@ namespace media::ffmpeg {
             return;
         }
 
-        const std::string name = encoder->name ? encoder->name : "";
+        const std::string encoderName = encoder->name ? encoder->name : "";
 
-        encoderCtx->max_b_frames = 0;
-
-        if (name == "libx264") {
+        if (encoderName == "libx264") {
             av_opt_set(encoderCtx->priv_data, "preset", "veryfast", 0);
             av_opt_set(encoderCtx->priv_data, "tune", "zerolatency", 0);
         }
-        else if (name == "libx265") {
+        else if (encoderName == "libx265") {
             av_opt_set(encoderCtx->priv_data, "preset", "veryfast", 0);
             av_opt_set(encoderCtx->priv_data, "tune", "zerolatency", 0);
+        }
+        else if (encoderName == "libvpx" || encoderName == "libvpx-vp9") {
+            av_opt_set(encoderCtx->priv_data, "deadline", "realtime", 0);
+            av_opt_set(encoderCtx->priv_data, "cpu-used", "4", 0);
+        }
+        else if (encoderName == "libaom-av1") {
+            av_opt_set(encoderCtx->priv_data, "cpu-used", "6", 0);
+            av_opt_set(encoderCtx->priv_data, "row-mt", "1", 0);
         }
     }
 
