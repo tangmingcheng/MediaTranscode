6ca56030afcb751039fe711ee0b105b0c2c796dd
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-18 16:33:10 +0800

    add audio pipeline strategy interface

---
+++ src/internal/FFmpegAudioPipelineStrategy.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "internal/FFmpegAudioPipelineTypes.h"
+
+namespace media::ffmpeg {
+
+class IFFmpegAudioPipelineStrategy {
+public:
+    virtual ~IFFmpegAudioPipelineStrategy() = default;
+
+    virtual void reset() = 0;
+
+    virtual Status initialize(const FFmpegAudioPipelineConfig& config) = 0;
+    virtual Status processPacket(AVPacket* packet,
+                                 const FFmpegAudioPacketWrittenCallback& onPacketWritten) = 0;
+    virtual Status flush(const FFmpegAudioPacketWrittenCallback& onPacketWritten) = 0;
+
+    virtual Status sendFrame(AVFrame* frame);
+    virtual Result<int> receiveAndWritePackets(
+        const FFmpegAudioPacketWrittenCallback& onPacketWritten);
+
+    virtual bool isInitialized() const = 0;
+    virtual AudioMode mode() const = 0;
+    virtual AVStream* outputStream() const = 0;
+    virtual FFmpegAudioPacketProgress progress() const = 0;
+};
+
+} // namespace media::ffmpeg
