133d66152f2fec4f8f64501a160cf64524ee2afd
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-24 17:45:43 +0800

    refactor(types): remove legacy transcode types shim

--- src/media_transcode/MediaTranscodeTypes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Compatibility shim for source files not yet migrated to internal/TranscodeTypes.h.
-// Do not include this header from new code.
-#include "internal/TranscodeTypes.h"
