c7cd9e304af42197b362553d2eb38066841ba751
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 11:27:22 +0800

    Remove video transcode pipeline compatibility header

--- src/internal/FFmpegVideoTranscodePipeline.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "internal/core/video/FFmpegVideoProcessingPipeline.h"
-
-namespace media::ffmpeg {
-
-using FFmpegVideoTranscodePipeline = FFmpegVideoProcessingPipeline;
-
-} // namespace media::ffmpeg
