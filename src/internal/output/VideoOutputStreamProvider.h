16bbc19ec09f269951ec1282c924a6c0071227ec
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 16:56:01 +0800

    Forward video capability header

--- src/internal/output/VideoOutputStreamProvider.h
+++ src/internal/output/VideoOutputStreamProvider.h
@@ -1,26 +1,3 @@
 #pragma once
 
-#include "media_transcode/Result.h"
-
-extern "C" {
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-}
-
-namespace media::ffmpeg {
-
-class VideoOutputStreamProvider {
-public:
-    virtual ~VideoOutputStreamProvider() = default;
-
-    VideoOutputStreamProvider(const VideoOutputStreamProvider&) = delete;
-    VideoOutputStreamProvider& operator=(const VideoOutputStreamProvider&) = delete;
-
-    virtual bool requiresGlobalHeader() const = 0;
-    virtual Result<AVStream*> createVideoStream(AVCodecContext* encoderCtx) = 0;
-
-protected:
-    VideoOutputStreamProvider() = default;
-};
-
-} // namespace media::ffmpeg
+#include "internal/output/capabilities/video/VideoOutputStreamProvider.h"
