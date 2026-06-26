ea3bc820e3ec8d7328897cdf1d3f2a7e4414e654
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 17:11:07 +0800

    Forward RTP URL builder header

--- src/internal/output/FFmpegRtpUrlBuilder.h
+++ src/internal/output/FFmpegRtpUrlBuilder.h
@@ -1,22 +1,3 @@
 #pragma once
 
-#include <string>
-
-namespace media::ffmpeg {
-
-struct FFmpegRtpOutputConfig {
-    std::string host;
-    int rtpPort = 0;
-    int rtcpPort = 0;
-    int localRtpPort = 0;
-    int localRtcpPort = 0;
-    int packetSize = 1200;
-    std::string sdpOutputPath;
-};
-
-class FFmpegRtpUrlBuilder {
-public:
-    static std::string build(const FFmpegRtpOutputConfig& config);
-};
-
-} // namespace media::ffmpeg
+#include "internal/output/builders/rtp/FFmpegRtpUrlBuilder.h"
