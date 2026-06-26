8d1f5dd925245d619f0cf6344f2208aa22c750a5
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 19:33:24 +0800

    Remove legacy muxer output node alias header

--- src/internal/output/FFmpegMuxerOutputNode.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "internal/output/nodes/file/FFmpegFileOutputNode.h"
-
-namespace media::ffmpeg {
-
-using FFmpegMuxerOutputNode = FFmpegFileOutputNode;
-
-} // namespace media::ffmpeg
