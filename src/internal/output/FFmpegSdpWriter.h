9460860cd5c03953d33cc4e134dc3d10ac03bcb0
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 17:28:15 +0800

    Forward SDP writer header

--- src/internal/output/FFmpegSdpWriter.h
+++ src/internal/output/FFmpegSdpWriter.h
@@ -1,19 +1,3 @@
 #pragma once
 
-#include "media_transcode/Result.h"
-
-#include <string>
-
-extern "C" {
-#include <libavformat/avformat.h>
-}
-
-namespace media::ffmpeg {
-
-class FFmpegSdpWriter {
-public:
-    static Status save(AVFormatContext* formatContext,
-                       const std::string& path);
-};
-
-} // namespace media::ffmpeg
+#include "internal/output/writers/sdp/FFmpegSdpWriter.h"
