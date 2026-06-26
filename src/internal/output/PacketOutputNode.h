d80e4dabad22b232d8a720b09e7edd36e77aeb92
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-26 16:57:47 +0800

    Forward packet output node header

--- src/internal/output/PacketOutputNode.h
+++ src/internal/output/PacketOutputNode.h
@@ -1,24 +1,3 @@
 #pragma once
 
-#include "media_transcode/Result.h"
-
-extern "C" {
-#include <libavcodec/packet.h>
-}
-
-namespace media::ffmpeg {
-
-class PacketOutputNode {
-public:
-    virtual ~PacketOutputNode() = default;
-
-    PacketOutputNode(const PacketOutputNode&) = delete;
-    PacketOutputNode& operator=(const PacketOutputNode&) = delete;
-
-    virtual Status pushPacket(AVPacket* packet) = 0;
-
-protected:
-    PacketOutputNode() = default;
-};
-
-} // namespace media::ffmpeg
+#include "internal/graph/packet/PacketOutputNode.h"
