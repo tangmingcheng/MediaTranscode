ff186ffa6595b4257d65f6855fc96a43ce3a27cc
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-28 19:19:34 +0800

    Route local transcode preset to DAG builder

--- src/internal/graph/preset/MediaPipelinePreset.cpp
+++ src/internal/graph/preset/MediaPipelinePreset.cpp
@@ -1,5 +1,7 @@
 #include "internal/graph/preset/MediaPipelinePreset.h"
 
+#include "internal/graph/builder/local/MediaLocalFileTranscodeGraphBuilder.h"
+
 #include <utility>
 
 namespace media::ffmpeg::graph {
@@ -10,8 +12,8 @@ namespace media::ffmpeg::graph {
     switch (kind) {
     case MediaPipelinePresetKind::LocalFileRemux:
         return createLocalFileRemux(options);
-    case MediaPipelinePresetKind::LocalFileTranscodeSkeleton:
-        return createLocalFileTranscodeSkeleton(options);
+    case MediaPipelinePresetKind::LocalFileTranscode:
+        return createLocalFileTranscode(options);
     case MediaPipelinePresetKind::RealtimeRtpSkeleton:
         return createRealtimeRtpSkeleton(options);
     default:
@@ -54,56 +56,16 @@ namespace media::ffmpeg::graph {
     return ::media::Result<MediaGraph>::success(std::move(graph));
 }
 
-::media::Result<MediaGraph> MediaPipelinePreset::createLocalFileTranscodeSkeleton(const MediaPipelinePresetOptions& options)
+::media::Result<MediaGraph> MediaPipelinePreset::createLocalFileTranscode(const MediaPipelinePresetOptions& options)
 {
-    if (options.inputUrl.empty() || options.outputUrl.empty()) {
-        return ::media::Result<MediaGraph>::failure(
-            ::media::ErrorInfo::invalidArgument("LocalFileTranscodeSkeleton requires inputUrl and outputUrl"));
-    }
+    MediaLocalFileTranscodeGraphBuilderOptions builderOptions;
+    builderOptions.inputUrl = options.inputUrl;
+    builderOptions.outputUrl = options.outputUrl;
+    builderOptions.outputFormat = options.outputFormat;
+    builderOptions.includeAudio = options.includeAudio;
+    builderOptions.includeVideo = options.includeVideo;
 
-    MediaGraph graph;
-
-    const MediaNodeId fileInput = graph.addNode(MediaNodeKind::FileInput, "file-input");
-    const MediaNodeId fileOutput = graph.addNode(MediaNodeKind::FileOutput, "file-output");
-    const MediaNodeId demux = graph.addNode(MediaNodeKind::Demux, "demux");
-    const MediaNodeId split = graph.addNode(MediaNodeKind::StreamSplit, "stream-split");
-    const MediaNodeId videoDecode = graph.addNode(MediaNodeKind::VideoDecode, "video-decode");
-    const MediaNodeId videoEncode = graph.addNode(MediaNodeKind::VideoEncode, "video-encode");
-    const MediaNodeId audioCopy = graph.addNode(MediaNodeKind::AudioCopy, "audio-copy");
-    const MediaNodeId mux = graph.addNode(MediaNodeKind::FileMux, "file-mux");
-
-    graph.setNodeOption(fileInput, "url", options.inputUrl);
-    graph.setNodeOption(fileOutput, "url", options.outputUrl);
-    if (!options.outputFormat.empty()) {
-        graph.setNodeOption(fileOutput, "format", options.outputFormat);
-    }
-
-    graph.addOutputPort(fileInput, "format", MediaStreamKind::Metadata, MediaEdgeKind::Metadata, MediaPayloadKind::FormatContext);
-    graph.addOutputPort(fileOutput, "format", MediaStreamKind::Metadata, MediaEdgeKind::Metadata, MediaPayloadKind::FormatContext);
-    graph.addInputPort(demux, "input", MediaStreamKind::Metadata, MediaEdgeKind::Metadata, MediaPayloadKind::FormatContext);
-    graph.addOutputPort(demux, "packet", MediaStreamKind::Any, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, true, true);
-    graph.addInputPort(split, "packet", MediaStreamKind::Any, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, true, true);
-    graph.addOutputPort(split, "video", MediaStreamKind::Video, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, false, true);
-    graph.addOutputPort(split, "audio", MediaStreamKind::Audio, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, false, true);
-    graph.addInputPort(videoDecode, "packet", MediaStreamKind::Video, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, false, true);
-    graph.addOutputPort(videoDecode, "frame", MediaStreamKind::Video, MediaEdgeKind::RawFrame, MediaPayloadKind::Frame, false, true);
-    graph.addInputPort(videoEncode, "frame", MediaStreamKind::Video, MediaEdgeKind::RawFrame, MediaPayloadKind::Frame, false, true);
-    graph.addOutputPort(videoEncode, "packet", MediaStreamKind::Video, MediaEdgeKind::EncodedPacket, MediaPayloadKind::Packet, false, true);
-    graph.addInputPort(audioCopy, "packet", MediaStreamKind::Audio, MediaEdgeKind::InputPacket, MediaPayloadKind::Packet, false, true);
-    graph.addOutputPort(audioCopy, "packet", MediaStreamKind::Audio, MediaEdgeKind::CopiedPacket, MediaPayloadKind::Packet, false, true);
-    graph.addInputPort(mux, "format", MediaStreamKind::Metadata, MediaEdgeKind::Metadata, MediaPayloadKind::FormatContext);
-    graph.addInputPort(mux, "packet", MediaStreamKind::Any, MediaEdgeKind::Unknown, MediaPayloadKind::Packet, true, true);
-
-    graph.connect(fileInput, "format", demux, "input", "input-format");
-    graph.connect(demux, "packet", split, "packet", "demux-to-split");
-    graph.connect(split, "video", videoDecode, "packet", "video-packet");
-    graph.connect(videoDecode, "frame", videoEncode, "frame", "video-frame");
-    graph.connect(videoEncode, "packet", mux, "packet", "video-encoded-packet");
-    graph.connect(split, "audio", audioCopy, "packet", "audio-packet");
-    graph.connect(audioCopy, "packet", mux, "packet", "audio-copy-packet");
-    graph.connect(fileOutput, "format", mux, "format", "output-format");
-
-    return ::media::Result<MediaGraph>::success(std::move(graph));
+    return MediaLocalFileTranscodeGraphBuilder::build(builderOptions);
 }
 
 ::media::Result<MediaGraph> MediaPipelinePreset::createRealtimeRtpSkeleton(const MediaPipelinePresetOptions&)
