6be2f986d3815aa47bdd7831f5063f602ef2791e
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-28 21:40:17 +0800

    Remove runloop dependency from pipeline runner header

--- src/internal/graph/runtime/streaming/MediaPipelineRunner.h
+++ src/internal/graph/runtime/streaming/MediaPipelineRunner.h
@@ -1,24 +1,23 @@
 #pragma once
 
 #include "internal/graph/preset/MediaPipelinePreset.h"
+#include "internal/graph/runtime/MediaGraphRuntime.h"
 #include "internal/graph/runtime/diagnostics/MediaGraphRuntimeReport.h"
-#include "internal/graph/runtime/runloop/MediaGraphRunLoop.h"
 #include "internal/graph/runtime/streaming/MediaStreamingSession.h"
 #include "media_transcode/Result.h"
 
 namespace media::ffmpeg::graph {
 
 struct MediaPipelineRunResult {
-    MediaGraphRunLoopResult runLoop;
+    MediaGraphRunResult run;
     MediaGraphRuntimeReport report;
 };
 
 class MediaPipelineRunner final {
 public:
-    static ::media::Result<MediaPipelineRunResult> runPresetUntilIdle(
+    static ::media::Result<MediaPipelineRunResult> runPreset(
         MediaPipelinePresetKind presetKind,
-        const MediaPipelinePresetOptions& options,
-        MediaGraphRunLoopConfig runLoopConfig = {});
+        const MediaPipelinePresetOptions& options);
 };
 
 } // namespace media::ffmpeg::graph
