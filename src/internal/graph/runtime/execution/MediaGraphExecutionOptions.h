10d8a4c7a388795b934667248fdbd0a259f60a6c
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-28 21:29:55 +0800

    Remove run loop config from execution options

--- src/internal/graph/runtime/execution/MediaGraphExecutionOptions.h
+++ src/internal/graph/runtime/execution/MediaGraphExecutionOptions.h
@@ -1,18 +1,16 @@
 #pragma once
 
 #include "internal/graph/model/MediaThreadingPolicy.h"
-#include "internal/graph/runtime/runloop/MediaGraphRunLoop.h"
 #include "internal/graph/runtime/execution/MediaGraphExecutionMode.h"
 
 namespace media::ffmpeg::graph {
 
 struct MediaGraphExecutionOptions {
-    MediaGraphExecutionMode mode = MediaGraphExecutionMode::SingleThreadedRunLoop;
+    MediaGraphExecutionMode mode = MediaGraphExecutionMode::SingleThreaded;
     MediaThreadingPolicy threadingPolicy;
-    MediaGraphRunLoopConfig runLoopConfig;
 
     bool autoRegisterDefaultNodes = true;
-    bool stopOnRunLoopCompletion = true;
+    bool stopOnCompletion = true;
 };
 
 } // namespace media::ffmpeg::graph
