c6b5d62ebaaf29860e7df15987abd8ac412323be
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-28 19:25:30 +0800

    Make local transcode builder the single local DAG builder

--- src/internal/graph/builder/local/LocalFileTranscodeGraphBuilder.h
+++ src/internal/graph/builder/local/LocalFileTranscodeGraphBuilder.h
@@ -1,22 +1,37 @@
 #pragma once
 
 #include "internal/graph/core/MediaGraph.h"
-#include "internal/graph/builder/MediaGraphBuilder.h"
+#include "media_transcode/Result.h"
+
+#include <cstddef>
+#include <string>
 
 namespace media::ffmpeg::graph {
 
 struct LocalFileTranscodeOptions {
+    std::string inputUrl;
+    std::string outputUrl;
+    std::string outputFormat;
+
     bool includeVideo = true;
     bool includeAudio = true;
     bool audioTranscode = false;
-    bool useHardwareTransfer = true;
+    bool useHardwareTransfer = false;
     bool includeMux = true;
     bool includeLifecycle = false;
+
+    std::size_t metadataQueueCapacity = 1;
+    std::size_t packetQueueCapacity = 256;
+    std::size_t frameQueueCapacity = 128;
+    std::size_t muxQueueCapacity = 256;
 };
 
-class LocalFileTranscodeGraphBuilder {
+class LocalFileTranscodeGraphBuilder final {
 public:
-    static MediaGraph build(const LocalFileTranscodeOptions& opt = {});
+    static ::media::Result<MediaGraph> build(const LocalFileTranscodeOptions& options);
+
+private:
+    static ::media::Status validate(const LocalFileTranscodeOptions& options);
 };
 
 } // namespace media::ffmpeg::graph
