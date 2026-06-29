a40115706a1047fa1bd363d603642429cdd3bf4b
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-29 19:11:19 +0800

    Add codec resolver hardware frame helper interface

---
+++ src/internal/graph/nodes/metadata/CodecResolverHardwareFrames.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "media_transcode/Status.h"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/buffer.h>
+#include <libavutil/pixfmt.h>
+}
+
+namespace media::ffmpeg::graph {
+
+::media::Status configureEncoderHardwareFrames(AVCodecContext* encoderContext,
+                                               AVBufferRef* hardwareDevice,
+                                               AVPixelFormat hardwareFormat,
+                                               AVPixelFormat softwareFormat,
+                                               int width,
+                                               int height,
+                                               int initialPoolSize);
+
+} // namespace media::ffmpeg::graph
