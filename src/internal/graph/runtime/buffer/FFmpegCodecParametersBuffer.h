7f05fc65d3961ba584311d381c0223d1c2a307bc
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-30 17:19:19 +0800

    graph: add codec parameters buffer header

---
+++ src/internal/graph/runtime/buffer/FFmpegCodecParametersBuffer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "internal/FFmpegRAII.h"
+#include "internal/graph/runtime/buffer/MediaBuffer.h"
+
+struct AVCodecParameters;
+
+namespace media::ffmpeg::graph {
+
+class FFmpegCodecParametersBuffer final : public MediaBuffer {
+public:
+    explicit FFmpegCodecParametersBuffer(::media::ffmpeg::CodecParametersPtr parameters);
+
+    MediaBufferType type() const noexcept override;
+
+    AVCodecParameters* parameters() noexcept;
+    const AVCodecParameters* parameters() const noexcept;
+
+private:
+    ::media::ffmpeg::CodecParametersPtr m_parameters;
+};
+
+} // namespace media::ffmpeg::graph
