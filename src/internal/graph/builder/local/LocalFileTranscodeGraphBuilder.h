f62c18634407462452be0605eba6072fdc68f02d
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-06-30 19:33:49 +0800

    graph: use canonical parameters in local transcode options

--- src/internal/graph/builder/local/LocalFileTranscodeGraphBuilder.h
+++ src/internal/graph/builder/local/LocalFileTranscodeGraphBuilder.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "internal/graph/core/MediaGraph.h"
+#include "internal/graph/model/MediaTranscodeParameters.h"
 #include "media_transcode/Result.h"
 
-#include <cstddef>
-#include <optional>
 #include <string>
 
 namespace media::ffmpeg::graph {
@@ -13,38 +12,7 @@ struct LocalFileTranscodeOptions {
     std::string inputUrl;
     std::string outputUrl;
     std::string outputFormat;
-    bool includeVideo = true;
-    bool includeAudio = true;
-    bool audioTranscode = false;
-    bool useHardwareTransfer = false;
-    bool includeMux = true;
-    bool includeLifecycle = false;
-    bool diagnosticLogEnabled = true;
-    std::string videoCodec;
-    std::string videoEncoder;
-    std::string rateControlMode = "auto";
-    std::string speedPreset;
-    std::string profile;
-    std::string tune;
-    std::string level;
-    std::optional<int> width;
-    std::optional<int> height;
-    std::optional<int> fpsNum;
-    std::optional<int> fpsDen;
-    std::optional<int> videoBitrateKbps;
-    std::optional<int> crf;
-    std::optional<int> quality;
-    std::optional<int> gop;
-    std::optional<int> maxBFrames;
-    bool disableHardware = false;
-    std::string audioCodec;
-    std::optional<int> audioBitrateKbps;
-    std::optional<int> audioSampleRate;
-    std::optional<int> audioChannels;
-    std::size_t metadataQueueCapacity = 1;
-    std::size_t packetQueueCapacity = 256;
-    std::size_t frameQueueCapacity = 128;
-    std::size_t muxQueueCapacity = 256;
+    MediaTranscodeParameterSet parameters;
 };
 
 class LocalFileTranscodeGraphBuilder final {
