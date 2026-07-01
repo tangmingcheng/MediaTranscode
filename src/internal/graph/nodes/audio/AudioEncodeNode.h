b256e1936367af92fd17fd6d748c39dab9b7b7c4
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-07-01 10:25:29 +0800

    graph: let audio encoder emit codec configuration

--- src/internal/graph/nodes/audio/AudioEncodeNode.h
+++ src/internal/graph/nodes/audio/AudioEncodeNode.h
@@ -13,7 +13,11 @@ protected:
     ::media::Status onProcess(MediaGraphExecutionContext& context) override;
 
 private:
+    ::media::Status emitEncoderConfig(MediaGraphExecutionContext& context, const MediaBufferRef& buffer);
     ::media::Status receivePackets(MediaGraphExecutionContext& context);
+
+private:
+    bool m_encoderConfigEmitted = false;
 };
 
 } // namespace media::ffmpeg::graph
