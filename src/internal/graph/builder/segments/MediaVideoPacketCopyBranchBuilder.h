d506dffe7f3268314e538ac09df3188ed3364c57
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-07-03 16:12:02 +0800

    Add video packet copy branch adapter

---
+++ src/internal/graph/builder/segments/MediaVideoPacketCopyBranchBuilder.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "internal/graph/core/MediaGraph.h"
+#include "internal/graph/model/MediaTranscodeParameters.h"
+#include "internal/graph/planner/MediaPipelinePlanner.h"
+#include "media_transcode/Result.h"
+
+#include <string>
+
+namespace media::ffmpeg::graph {
+
+struct MediaVideoPacketCopyBranchOptions {
+    std::string prefix = "video.copy";
+    MediaPipelinePlan plan;
+    MediaGraphQueueParameters queues;
+
+    MediaNodeId formatSourceNode = MediaNodeId::invalid();
+    std::string formatSourcePort = "format";
+
+    MediaNodeId packetSourceNode = MediaNodeId::invalid();
+    std::string packetSourcePort = "video";
+
+    MediaNodeId muxNode = MediaNodeId::invalid();
+    std::string muxCodecPort = "codec";
+    std::string muxPacketPort = "packet";
+};
+
+class MediaVideoPacketCopyBranchBuilder final {
+public:
+    static ::media::Result<void> build(MediaGraph& graph,
+                                       const MediaVideoPacketCopyBranchOptions& options);
+
+private:
+    MediaVideoPacketCopyBranchBuilder() = default;
+};
+
+} // namespace media::ffmpeg::graph
