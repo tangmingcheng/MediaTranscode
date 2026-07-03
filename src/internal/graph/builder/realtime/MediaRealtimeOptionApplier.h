9fdddf39956748b3f17871d53eb84d66d23382b1
tangmingcheng <45536906+tangmingcheng@users.noreply.github.com>
2026-07-03 17:04:02 +0800

    Add realtime option applier

---
+++ src/internal/graph/builder/realtime/MediaRealtimeOptionApplier.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "internal/graph/builder/realtime/MediaRealtimeGraphBuilder.h"
+#include "media_transcode/Result.h"
+
+namespace media::ffmpeg::graph {
+
+class MediaRealtimeOptionApplier final {
+public:
+    static ::media::Result<void> applyInputOptions(MediaGraph& graph,
+                                                   MediaNodeId nodeId,
+                                                   const MediaRealtimeGraphBuilderOptions& options);
+    static ::media::Result<void> applyOutputOptions(MediaGraph& graph,
+                                                    MediaNodeId nodeId,
+                                                    const MediaRealtimeGraphBuilderOptions& options);
+
+private:
+    MediaRealtimeOptionApplier() = default;
+};
+
+} // namespace media::ffmpeg::graph
